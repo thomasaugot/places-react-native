8595f6775e7038fa1c797f7ffd8640f5c42ade20
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-29 11:42:03 +0200

    naviagtion done and working, adding styles

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlaces from "./screens/AddPlaces";
+import Button from "./components/ui/Button";
+import { Colors } from "./constants/colors";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +14,35 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPLaces" component={AllPlaces} />
-          <Stack.Screen name="AddPLaces" component={AddPlaces} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
+          <Stack.Screen
+            name="AllPLaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your favorite places",
+              headerRight: ({ tintColor }) => (
+                <Button
+                  onPress={() => navigation.navigate("AddPLaces")}
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPLaces"
+            component={AddPlaces}
+            options={{ title: "Add a new place" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ components/ui/Button.js
@@ -0,0 +1,25 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const Button = ({ icon, size, color, onPress }) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: { opacity: 0.7 },
+});
